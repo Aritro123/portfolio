e14ae6262072085a96ded991555eceada9eb9579
Aritro123 <itsaritro990@gmail.com>
2024-04-14 00:50:38 +0530

    first comit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,17 +16,6 @@ const About = () => {
           web designer, poised at the brink of graduation from BCA. Hailing from
           Kolkata, Ive honed my skills in React, Redux, Tailwind CSS, HTML, and
           CSS, with a strong foundation in version control using GitHub.
-          Throughout my academic tenure, Ive been fascinated by the fusion of
-          creativity and technology, which has fueled my passion for web design.
-          With each project, Ive delved deeper into crafting immersive user
-          experiences, leveraging the power of React and Redux to build dynamic
-          interfaces. My attention to detail and penchant for clean, efficient
-          code have driven me to continuously refine my skills, ensuring optimal
-          performance and scalability. Tailwind CSS has been instrumental in
-          streamlining my design process, allowing me to create visually
-          stunning layouts with ease. From conceptualization to deployment, I
-          thrive on the collaborative nature of web development, relishing the
-          opportunity to transform ideas into tangible digital experiences
         </p>
         <br />
         <p className="text-xl">
@@ -34,12 +23,8 @@ const About = () => {
           student to professional, ready to immerse myself fully in the world of
           web design. I`m on the lookout for exciting job opportunities and
           projects where I can apply my skills and creativity to make a
-          meaningful impact. Whether it`s designing sleek, responsive interfaces
-          or optimizing user interactions, I`m committed to pushing the
-          boundaries of what`s possible in web design. I invite you to explore
-          my portfolio and connect with me to discuss how we can collaborate to
-          create something extraordinary. Let`s embark on this journey together,
-          shaping the future of web design one project at a time.
+          meaningful impact. so Let`s embark on this journey together, shaping
+          the future of web design one project at a time.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div
       name="Home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800 via-black "
+      className="h-screen w-full bg-gradient-to-b from-black to-gray-800 via-black"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-white">
-        <div className="flex flex-col justify-center h-full">
+        <div className="flex flex-col justify-center h-full mt-[70px]">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I`m Aritro
           </h2>
@@ -25,12 +25,8 @@ const Home = () => {
             Welcome to my portfolio! I`m a driven React developer from Kolkata,
             currently wrapping up my BCA degree. Excited to leap into the vast
             realm of web development, I bring a solid understanding of React and
-            a hunger for continuous learning. With a focus on crafting intuitive
-            interfaces and optimizing user experiences, I`m poised to tackle
-            challenges and deliver impactful solutions. Eager to contribute my
-            skills and creativity to dynamic projects, I`m actively seeking
-            opportunities to grow and thrive in the ever-evolving tech
-            landscape. Let`s connect and create something remarkable together!
+            a hunger for continuous learning. I`m actively seeking opportunities
+            to grow and thrive in the ever-evolving tech landscape
           </p>
           <div>
             <Link
